2f0261a586e448542e86aed35b566bdd84a67abd
Kevin Wong <kkwokwai22@gmail.com>
2018-07-26 12:23:04 -0700

    add stringCompression

---
+++ stringCompression.js
@@ -0,0 +1,21 @@
+// Description:StringCompression
+// Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not become smaller than the original string, your method should return the original string.
+
+/**
+ * @param {string} string
+ * @return {string}
+ */
+function stringCompression(string) {
+	let stringObject = {};
+	let stringArr = string.split('');
+	stringArr.forEach(function(char) {
+		if (!stringObject.hasOwnProperty(char)) {
+			stringObject[char] = 1;
+		} else {
+			stringObject[char]++;
+		}
+	});
+	return stringObject;
+}
+
+stringCompression('aabcccccaaa');
