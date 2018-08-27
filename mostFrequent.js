ae455e2c2f5063874d5c0c25bd6e85e1bb5afc07
Kevin Wong <kkwokwai22@gmail.com>
2018-08-27 15:12:33 -0700

    most frequent

---
+++ mostFrequent.js
@@ -0,0 +1,35 @@
+// Most Frequent item occur in the Array
+
+// # NOTE: The following input values will be used for testing your solution.
+// # most_frequent(list1) should return 1
+let list1 = [1, 3, 1, 3, 2, 1];
+// # most_frequent(list2) should return 3
+let list2 = [3, 3, 1, 3, 2, 1];
+// # most_frequent(list3) should return None
+let list3 = [];
+// # most_frequent(list4) should return 0
+let list4 = [0];
+// # most_frequent(list5) should return -1
+let list5 = [0, -1, 10, 10, -1, 10, -1, -1, -1, 1];
+
+function mostFrequent(arr) {
+	let itemOccur = {};
+	if (arr.length === 0) {
+		return null;
+	}
+	arr.forEach(item => {
+		if (!itemOccur[item]) {
+			itemOccur[item] = 1;
+		} else {
+			itemOccur[item]++;
+		}
+	});
+	let maxOccur = 0;
+	let itemResult;
+	for (let item in itemOccur) {
+		if (itemOccur[item] > maxOccur) {
+			itemResult = item;
+		}
+	}
+	return itemResult;
+}
